2deecdb7179dffe28ff2d4f08578658af67d5c73
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-08-02 18:50:41 -0600

    chore: Add RightSidebar component and update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Omar" };
+  const loggedIn = {
+    firstName: "Omar",
+    lastName: "Zuniga",
+    email: "contact@kasump.com",
+  };
 
   return (
     <section className="home">
@@ -21,7 +26,14 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
