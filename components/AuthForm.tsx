2a990b2be81e5a3090b43fa559b129da48d7d59d
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-08-03 00:42:53 -0600

    chore: Update package.json and types with node-appwrite dependency and add city field to authFormSchema

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,8 +11,13 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import CustomInput from "./CustomInput";
 import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signUp } from "@/lib/actions/user.actions";
+import SignIn from "@/app/(auth)/sign-in/page";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
+
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -28,13 +33,30 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    console.log(values);
-    setIsLoading(false);
-  }
+
+    try {
+      //Sign Up wiht appwrite & create plaid token
+
+      if (type == "sign-up") {
+        const newUser = await signUp(data);
+        setUser(newUser);
+      }
+
+      if (type == "sign-in") {
+        //const response = await SignIn({
+        // email: data.email,
+        //  password: data.password,
+        //});
+        //if (response) router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
@@ -93,6 +115,13 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific address"
+                    type="text"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city"
                     type="text"
                   />
 
